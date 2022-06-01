eb65822818d69c87f31aac826c8ee54ad70daa3f
Víctor Díaz <diaz.victorm@pucp.edu.pe>
2022-05-31 19:39:01 -0500

    Initial commit

---
+++ src/main/java/com/example/lab6_gtics_20221_g4/Lab6GTics20221G4Application.java
@@ -0,0 +1,13 @@
+package com.example.lab6_gtics_20221_g4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6GTics20221G4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6GTics20221G4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab6_gtics_20221_g4/Lab6GTics20221G4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6_gtics_20221_g4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6GTics20221G4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
